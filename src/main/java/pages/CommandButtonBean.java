ef4977958dc84a08fd2087a9b4864fd349dc9ac2
krisxl007 <krisxl1987@gmail.com>
2017-09-16 11:45:26 +1000

    add commandButton.xhtml and CommandButtonBean

---
+++ src/main/java/pages/CommandButtonBean.java
@@ -0,0 +1,18 @@
+package pages;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CommandButtonBean {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
